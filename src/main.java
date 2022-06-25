79e190dfa3b9a27270e8f6e16d0ab17660233c7f
Lee Shawver <leehshawver@gmail.com>
2022-06-24 20:43:24 -0600

    First commit for Lab 1A

---
+++ src/main.java
@@ -0,0 +1,21 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int availableSeats = 34;
+		double groceryCost = 4.34; 
+		char midInit = 'H';
+		boolean isHotOutside = true;
+		String firstName = "Lee";
+		String address = "209 Herdner Lane";
+		
+		System.out.println(availableSeats);
+		System.out.println(groceryCost);
+		System.out.println(midInit);
+		System.out.println(isHotOutside);
+		System.out.println(firstName);
+		System.out.println(availableSeats);
+	}
+
+}
